6ba0b1ca60fe6a44901e7cd6e766057a00b7ad48
carlonlv <36804238+carlonlv@users.noreply.github.com>
2020-01-24 00:22:00 -0500

    Update main.R

--- R/main.R
+++ R/main.R
@@ -311,8 +311,7 @@ predicting_sim <- function(object, dataset_max, dataset_avg, cores, write_result
 
   ## Do Simulation
   start_time <- proc.time()
-  #result <- parallel::mclapply(1:length(ts_names), svt_predicting_sim, object, dataset_max, dataset_avg, mc.cores = cores)
-  result <- lapply(1:length(ts_names), svt_predicting_sim, object, dataset_max, dataset_avg)
+  result <- parallel::mclapply(1:length(ts_names), svt_predicting_sim, object, dataset_max, dataset_avg, mc.cores = cores)
   end_time <- proc.time()
   print(end_time - start_time)
 
